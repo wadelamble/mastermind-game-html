2c68558651c92f94d2d2a2a94973602977e0a6dd
wadelamble <wadelamble@hotmail.com>
2020-09-12 14:05:41 -0700

    re add blob container stuff

--- index.js
+++ index.js
@@ -194,122 +194,11 @@ else {
     uploadStats();
     //show stats here
 }
-/*
+
 //azure blob storage globals
-const { BlobServiceClient } = require("@azure/storage-blob");
-const createContainerButton = document.getElementById("create-container-button");
-const deleteContainerButton = document.getElementById("delete-container-button");
-const selectButton = document.getElementById("select-button");
-const fileInput = document.getElementById("file-input");
-const listButton = document.getElementById("list-button");
-const deleteButton = document.getElementById("delete-button");
-const status = document.getElementById("status");
-const fileList = document.getElementById("file-list");
 
-const reportStatus = message => {
-    status.innerHTML += `${message}<br/>`;
-    status.scrollTop = status.scrollHeight;
-}
-// Update <placeholder> with your Blob service SAS URL string
-const blobSasUrl = "https://mileswadestorage.blob.core.windows.net/?sv=2019-12-12&ss=bfqt&srt=sco&sp=rwdlacupx&se=2020-09-08T06:24:59Z&st=2020-09-07T22:24:59Z&spr=https&sig=DVSxA0oAFsn%2B2m1bVhhfE6ZUWzXVRyyWoHEKN0hbqLI%3D";
 
-// Create a new BlobServiceClient
-const blobServiceClient = new BlobServiceClient(blobSasUrl);
 
-// Create a unique name for the container by 
-// appending the current time to the file name
-const containerName = "mw-mastermind-blob-container"
-
-// Get a container client from the BlobServiceClient
-const containerClient = blobServiceClient.getContainerClient(containerName);
-
-const createContainer = async () => {
-    try {
-        reportStatus(`Creating container "${containerName}"...`);
-        await containerClient.create();
-        reportStatus(`Done.`);
-    } catch (error) {
-        reportStatus(error.message);
-    }
-};
-
-const deleteContainer = async () => {
-    try {
-        reportStatus(`Deleting container "${containerName}"...`);
-        await containerClient.delete();
-        reportStatus(`Done.`);
-    } catch (error) {
-        reportStatus(error.message);
-    }
-};
-
-createContainerButton.addEventListener("click", createContainer);
-deleteContainerButton.addEventListener("click", deleteContainer);
-
-const listFiles = async () => {
-    fileList.size = 0;
-    fileList.innerHTML = "";
-    try {
-        reportStatus("Retrieving file list...");
-        let iter = containerClient.listBlobsFlat();
-        let blobItem = await iter.next();
-        while (!blobItem.done) {
-            fileList.size += 1;
-            fileList.innerHTML += `<option>${blobItem.value.name}</option>`;
-            blobItem = await iter.next();
-        }
-        if (fileList.size > 0) {
-            reportStatus("Done.");
-        } else {
-            reportStatus("The container does not contain any files.");
-        }
-    } catch (error) {
-        reportStatus(error.message);
-    }
-};
-
-listButton.addEventListener("click", listFiles);
-
-const uploadFiles = async () => {
-    try {
-        reportStatus("Uploading files...");
-        const promises = [];
-        for (const file of fileInput.files) {
-            const blockBlobClient = containerClient.getBlockBlobClient(file.name);
-            promises.push(blockBlobClient.uploadBrowserData(file));
-        }
-        await Promise.all(promises);
-        reportStatus("Done.");
-        listFiles();
-    }
-    catch (error) {
-            reportStatus(error.message);
-    }
-}
-
-selectButton.addEventListener("click", () => fileInput.click());
-fileInput.addEventListener("change", uploadFiles);
-
-const deleteFiles = async () => {
-    try {
-        if (fileList.selectedOptions.length > 0) {
-            reportStatus("Deleting files...");
-            for (const option of fileList.selectedOptions) {
-                await containerClient.deleteBlob(option.text);
-            }
-            reportStatus("Done.");
-            listFiles();
-        } else {
-            reportStatus("No files selected.");
-        }
-    } catch (error) {
-        reportStatus(error.message);
-    }
-};
-
-deleteButton.addEventListener("click", deleteFiles);
-
-*/
 
 //
 // end globals
@@ -317,6 +206,7 @@ deleteButton.addEventListener("click", deleteFiles);
 
 window.startMenu = function startMenu() {
     drawStartButtons();
+    setupBlobs();
 }
 
 window.startGame = function startGame() {
@@ -334,6 +224,136 @@ window.startStatPage = function startStatPage() {
     //document.getElementById("timesVisited").innerHTML = "Times you've visited this site: " + userStats.timesVisited;
 }
 
+function setupBlobs() {
+    const { BlobServiceClient } = require("@azure/storage-blob");
+    const createContainerButton = document.getElementById("create-container-button");
+    const deleteContainerButton = document.getElementById("delete-container-button");
+    const selectButton = document.getElementById("select-button");
+    const fileInput = document.getElementById("file-input");
+    const listButton = document.getElementById("list-button");
+    const deleteButton = document.getElementById("delete-button");
+    const status = document.getElementById("status");
+    const fileList = document.getElementById("file-list");
+
+    // Update <placeholder> with your Blob service SAS URL string
+    const blobSasUrl = "https://mileswadestorage.blob.core.windows.net/?sv=2019-12-12&ss=bfqt&srt=sco&sp=rwdlacupx&se=2020-09-13T04:53:53Z&st=2020-09-12T20:53:53Z&spr=https,http&sig=0cR8UDG1GAFpT2ig%2FMj%2Bmu2I0yVMfl21U1RgLVKWjpg%3D";
+    
+
+    // Create a new BlobServiceClient
+    const blobServiceClient = new BlobServiceClient(blobSasUrl);
+
+    // Create a unique name for the container by 
+    // appending the current time to the file name
+    const containerName = "mw-mastermind-blob-container-random"
+
+    // Get a container client from the BlobServiceClient
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+
+
+    const reportStatus = message => {
+        status.innerHTML += `${message}<br/>`;
+        status.scrollTop = status.scrollHeight;
+    }
+
+    const createContainer = async () => {
+        try {
+            reportStatus(`Creating container "${containerName}"...`);
+            await containerClient.create();
+            reportStatus(`Done.`);
+        } catch (error) {
+            reportStatus(error.message);
+        }
+    };
+
+    const deleteContainer = async () => {
+        try {
+            reportStatus(`Deleting container "${containerName}"...`);
+            await containerClient.delete();
+            reportStatus(`Done.`);
+        } catch (error) {
+            reportStatus(error.message);
+        }
+    };
+
+
+
+    const listFiles = async () => {
+        fileList.size = 0;
+        fileList.innerHTML = "";
+        try {
+            reportStatus("Retrieving file list...");
+            let iter = containerClient.listBlobsFlat();
+            let blobItem = await iter.next();
+            while (!blobItem.done) {
+                fileList.size += 1;
+                fileList.innerHTML += `<option>${blobItem.value.name}</option>`;
+                blobItem = await iter.next();
+            }
+            if (fileList.size > 0) {
+                reportStatus("Done.");
+            } else {
+                reportStatus("The container does not contain any files.");
+            }
+        } catch (error) {
+            reportStatus(error.message);
+        }
+    };
+
+
+    const uploadFiles = async () => {
+        try {
+            reportStatus("Uploading files...");
+            const promises = [];
+            for (const file of fileInput.files) {
+                const blockBlobClient = containerClient.getBlockBlobClient(file.name);
+                promises.push(blockBlobClient.uploadBrowserData(file));
+            }
+            await Promise.all(promises);
+            reportStatus("Done.");
+            listFiles();
+        }
+        catch (error) {
+                reportStatus(error.message);
+        }
+    }
+
+
+
+    const deleteFiles = async () => {
+        try {
+            if (fileList.selectedOptions.length > 0) {
+                reportStatus("Deleting files...");
+                for (const option of fileList.selectedOptions) {
+                    await containerClient.deleteBlob(option.text);
+                }
+                reportStatus("Done.");
+                listFiles();
+            } else {
+                reportStatus("No files selected.");
+            }
+        } catch (error) {
+            reportStatus(error.message);
+        }
+    };
+
+    createContainerButton.addEventListener("click", createContainer);
+    deleteContainerButton.addEventListener("click", deleteContainer);
+
+    listButton.addEventListener("click", listFiles);
+
+    selectButton.addEventListener("click", () => fileInput.click());
+    fileInput.addEventListener("change", uploadFiles);
+
+    deleteButton.addEventListener("click", deleteFiles);
+
+    
+
+    
+    
+
+
+
+}
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
